6bf18a987afd43a359a4a13dcb058193f39ee8f5
dvanrhenen <d.vanrhenen@vwt.digital>
2019-10-04 14:05:57 +0200

    Added new api-key as security. DAT-1002

--- api_server/openapi_server/controllers/security_controller_.py
+++ api_server/openapi_server/controllers/security_controller_.py
@@ -1,33 +1,50 @@
-import config
-import logging
-from jwkaas import JWKaas
+import io
+import json
 
-from flask import g
+from google.cloud import kms_v1
 
-my_jwkaas = None
-my_e2e_jwkaas = None
+def decrypt():
+    project_id = os.environ['PROJECT_ID']
+    location_id = "europe"
+    key_ring_id = "api-key"
+    crypto_key_id = "api-key-monitoring"
 
-if hasattr(config, 'OAUTH_JWKS_URL'):
-    my_jwkaas = JWKaas(config.OAUTH_EXPECTED_AUDIENCE,
-                       config.OAUTH_EXPECTED_ISSUER,
-                       jwks_url=config.OAUTH_JWKS_URL)
+    # Creates an API client for the KMS API.
+    client = kms_v1.KeyManagementServiceClient()
+
+    # The resource name of the CryptoKey.
+    name = client.crypto_key_path_path(project_id, location_id, key_ring_id,
+                                       crypto_key_id)
+
+    # Use the KMS API to decrypt the data.
+    with io.open('api-credentials.enc', "rb") as file:
+        c_text = file.read()
+
+    response = client.decrypt(name, c_text)
+    secret_dict = response.plaintext.decode("utf-8").replace('\n', '')
+
+    return secret_dict
 
 
-def info_from_OAuth2AzureAD(token):
+decrypted_apikey = None
+decrypted_apikey = decrypt()
+
+
+def info_from_ApiKeyAuth(api_key, required_scopes):
     """
-    Validate and decode token.
+    Check and retrieve authentication information from api_key.
     Returned value will be passed in 'token_info' parameter of your operation function, if there is one.
     'sub' or 'uid' will be set in 'user' parameter of your operation function, if there is one.
-    'scope' or 'scopes' will be passed to scope validation function.
 
-    :param token Token provided by Authorization header
-    :type token: str
-    :return: Decoded token information or None if token is invalid
+    :param api_key API key provided by Authorization header
+    :type api_key: str
+    :param required_scopes Always None. Used for other authentication method
+    :type required_scopes: None
+    :return: Information attached to provided api_key or None if api_key is invalid or does not allow access to called API
     :rtype: dict | None
     """
-    result = my_jwkaas.get_connexion_token_info(token)
 
-    if result is not None:
-        g.user = result.get('upn', '')
+    if decrypted_apikey is not None and api_key == decrypted_apikey:
+        return {'uid': 'monitoring'}
 
-    return result
+    return None
