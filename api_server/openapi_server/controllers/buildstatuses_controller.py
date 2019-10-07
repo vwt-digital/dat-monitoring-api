e01b0879222b30110204612c009bdff1fdf01466
dvanrhenen <d.vanrhenen@vwt.digital>
2019-10-07 17:20:05 +0200

    Updated API for new build statuses. DAT-1015

--- api_server/openapi_server/controllers/buildstatuses_controller.py
+++ api_server/openapi_server/controllers/buildstatuses_controller.py
@@ -6,17 +6,18 @@ from flask import make_response
 
 from google.cloud import datastore
 
-from openapi_server.models.build_status import BuildStatus  # noqa: E501
+from openapi_server.models.build_other_status import BuildOtherStatus  # noqa: E501
+from openapi_server.models.build_trigger_status import BuildTriggerStatus  # noqa: E501
 from openapi_server import util
 
 
-def build_statuses_get():  # noqa: E501
-    """Get all build statuses
+def build_statuses_triggers_get():  # noqa: E501
+    """Get all build trigger statuses
 
-    Get a list of all build statuses # noqa: E501
+    Get a list of all build trigger statuses # noqa: E501
 
 
-    :rtype: List[BuildStatus]
+    :rtype: List[BuildTriggerStatus]
     """
     # Get entity from kind
     db_client = datastore.Client()
@@ -39,15 +40,15 @@ def build_statuses_get():  # noqa: E501
     return make_response(jsonify([]), 204)
 
 
-def build_statuses_branch_branch_get(branch):  # noqa: E501
-    """Get all branch build statuses
+def build_statuses_triggers_branch_branch_get(branch):  # noqa: E501
+    """Get all build trigger statuses by branch
 
-    Get a list of all build statuses from a specific branch # noqa: E501
+    Get a list of all build trigger statuses from a specific branch # noqa: E501
 
     :param branch: A unique branch identifier
     :type branch: str
 
-    :rtype: List[BuildStatus]
+    :rtype: List[BuildTriggerStatus]
     """
     branches = ['master', 'develop']
     if branch in branches:
@@ -75,43 +76,60 @@ def build_statuses_branch_branch_get(branch):  # noqa: E501
     return make_response(jsonify('Branch not of correct type'), 404)
 
 
-def build_statuses_project_project_id_get(project_id, branch=None):  # noqa: E501
-    """Get all project build statuses
+def build_statuses_other_get():  # noqa: E501
+    """Get all other build statuses
 
-    Get a list of all build statuses from a specific project # noqa: E501
+    Get a list of all other build statuses # noqa: E501
 
-    :param project_id: A unique project identifier
-    :type project_id: str
-    :param branch: A specific branch
-    :type branch: str
 
-    :rtype: List[BuildStatus]
+    :rtype: List[BuildOtherStatus]
     """
-    if branch:
-        branches = ['master', 'develop']
-        if branch not in branches:
-            return make_response(jsonify('Branch not of correct type'), 404)
-
-    # Get entity from kind
     db_client = datastore.Client()
-    query = db_client.query(kind='CloudBuilds')
-    query.add_filter('project_id', '=', project_id)
-
-    if branch:
-        query.add_filter('branch', '=', branch)
+    query = db_client.query(kind='CloudBuildsOther')
     db_data = query.fetch()
 
     # Return results
     if db_data:
         result = [{
-            'branch': ap['branch'],
-            'git_source': ap['git_source'],
-            'organization': ap['organization'],
-            'project_id': ap['project_id'],
-            'repo_name': ap['repo_name'],
-            'status': ap['status'],
-            'updated': ap['updated']
+            'finish_time': ap['finishTime'] if 'finishTime' in ap else '',
+            'start_time': ap['startTime'] if 'startTime' in ap else '',
+            'id': ap['id'] if 'id' in ap else '',
+            'project_id': ap['projectId'] if 'projectId' in ap else '',
+            'status': ap['status'] if 'status' in ap else ''
         } for ap in db_data]
         return result
 
     return make_response(jsonify([]), 204)
+
+
+def build_statuses_other_status_get(status):  # noqa: E501
+    """Get all other build statuses by status
+
+    Get a list of all other build statuses with a specific status # noqa: E501
+
+    :param status: A unique status identifier
+    :type status: str
+
+    :rtype: List[BuildOtherStatus]
+    """
+    statuses = ['failing', 'pending', 'passing']
+    if status in statuses:
+        db_client = datastore.Client()
+        query = db_client.query(kind='CloudBuildsOther')
+        query.add_filter('status', '=', status)
+        db_data = query.fetch()
+
+        # Return results
+        if db_data:
+            result = [{
+                'finish_time': ap['finishTime'] if 'finishTime' in ap else '',
+                'start_time': ap['startTime'] if 'startTime' in ap else '',
+                'id': ap['id'] if 'id' in ap else '',
+                'project_id': ap['projectId'] if 'projectId' in ap else '',
+                'status': ap['status'] if 'status' in ap else ''
+            } for ap in db_data]
+            return result
+
+        return make_response(jsonify([]), 204)
+
+    return make_response(jsonify('Status not of correct type'), 404)
--- api_server/openapi_server/models/__init__.py
+++ api_server/openapi_server/models/__init__.py
@@ -3,4 +3,4 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from openapi_server.models.build_status import BuildStatus
+from openapi_server.models.build_trigger_status import BuildTriggerStatus
