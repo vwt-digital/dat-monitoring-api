1613205a7ec9d2969257fd7df3eb061caeee8657
dvanrhenen <d.vanrhenen@vwt.digital>
2019-10-23 11:44:10 +0200

    Added population of error count kind to consume_errors_func. DAT-1113

--- functions/consume_errors_func/dbprocessor/__init__.py
+++ functions/consume_errors_func/dbprocessor/__init__.py
@@ -1,7 +1,9 @@
-from google.cloud import datastore
-import config
-import uuid
 import datetime
+import uuid
+import config
+
+from google.cloud import datastore
+
 
 class DBProcessor(object):
     def __init__(self):
@@ -9,8 +11,19 @@ class DBProcessor(object):
         pass
 
     def process(self, payload):
-        entity_key = self.client.key(config.DB_ERROR_REPORTING_KIND, str(uuid.uuid4()))
+        entity_key = self.client.key(config.DB_ERROR_REPORTING_KIND,
+                                     str(uuid.uuid4()))
         entity = self.client.get(entity_key)
+
+        payload['project_id'] = ''
+
+        if 'logName' in payload:
+            payload['project_id'] = payload['logName'].split('/')[1]
+        elif 'resource' in payload \
+            and 'labels' in payload['resource'] \
+                and 'project_id' in payload['resource']['labels']:
+            payload['project_id'] = \
+                payload['resource']['labels']['project_id']
 
         if entity is None:
             entity = datastore.Entity(key=entity_key)
@@ -18,12 +31,39 @@ class DBProcessor(object):
         self.populate_from_payload(entity, payload)
         self.client.put(entity)
 
+        self.populate_count_from_payload(payload)
+
     @staticmethod
     def populate_from_payload(entity, payload):
         entity.update({
-            'insertId': payload['insertId'] if 'insertId' in payload else '',
-            'logName': payload['logName'] if 'logName' in payload else '',
-            'receiveTimestamp': payload['receiveTimestamp'] if 'receiveTimestamp' in payload else datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            'insert_id': payload['insertId'] if 'insertId' in payload else '',
+            'project_id': payload['project_id'],
+            'log_name': payload['logName'] if 'logName' in payload else '',
+            'receive_timestamp': payload['receiveTimestamp']
+            if 'receiveTimestamp' in payload else
+            datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             'resource': payload['resource'] if 'resource' in payload else '',
             'trace': payload['trace'] if 'trace' in payload else ''
         })
+
+    @staticmethod
+    def count_fpopulate_count_from_payloadrom_payload(self, payload):
+        error_key_name = '{}_{}'.format(payload['project_id'],
+                                        datetime.datetime.utcnow()
+                                        .strftime("%Y-%m-%d"))
+
+        error_count_key = self.client.key(config.DB_ERROR_COUNT_KIND,
+                                          error_key_name)
+        error_count = self.client.get(error_count_key)
+
+        if error_count:
+            error_count['count'] += 1
+            self.client.put(error_count)
+        else:
+            error_count = datastore.Entity(key=error_count_key)
+            error_count.update({
+                'count': 1,
+                'date': datetime.datetime.utcnow().strftime("%Y-%m-%d"),
+                'project_id': payload['project_id'],
+            })
+            self.client.put(error_count)
