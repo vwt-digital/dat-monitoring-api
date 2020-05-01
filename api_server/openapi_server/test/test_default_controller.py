5b4b77eefa566760913fd6cac79582827caa6728
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-01 11:35:12 +0200

    Regenerated unittest DAT-2597

--- api_server/openapi_server/test/test_default_controller.py
+++ api_server/openapi_server/test/test_default_controller.py
@@ -2,12 +2,12 @@
 
 from __future__ import absolute_import
 import unittest
-
-from flask import json
-from six import BytesIO
-
 import config
+
 from openapi_server.models.build_other_status import BuildOtherStatus  # noqa: E501
+from openapi_server.models.build_trigger_status import BuildTriggerStatus  # noqa: E501
+from openapi_server.models.error_report import ErrorReport  # noqa: E501
+from openapi_server.models.error_report_count import ErrorReportCount  # noqa: E501
 from openapi_server.test import BaseTestCase
 
 
@@ -15,16 +15,80 @@ class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
     def test_build_statuses_others_get(self):
-        """Test case for build_statusen_get
+        """Test case for build_statuses_others_get
 
-        Get all build statusen
+        Get last 20 other build statuses
         """
-        headers = {
+        headers = { 
             'Accept': 'application/json',
-            "x-api-key": config.API_KEY
+            "x-api-key": config.API_KEY,
         }
         response = self.client.open(
             '/build-statuses-others',
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_build_statuses_others_status_get(self):
+        """Test case for build_statuses_others_status_get
+
+        Get other build statuses by conditions
+        """
+        headers = { 
+            'Accept': 'application/json',
+            "x-api-key": config.API_KEY,
+        }
+        response = self.client.open(
+            '/build-statuses-others/{status}?days={days}&max_rows={max_rows}'.format(status='pending',days=7, max_rows=5),
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_build_statuses_triggers_get(self):
+        """Test case for build_statuses_triggers_get
+
+        Get all build trigger statuses
+        """
+        headers = { 
+            'Accept': 'application/json',
+            "x-api-key": config.API_KEY,
+        }
+        response = self.client.open(
+            '/build-statuses-triggers',
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_error_reports_counts_get(self):
+        """Test case for error_reports_counts_get
+
+        Get count of project errors reportings in last x days
+        """
+        headers = { 
+            'Accept': 'application/json',
+            "x-api-key": config.API_KEY,
+        }
+        response = self.client.open(
+            '/error-reports/counts?days={days}&max_rows={max_rows}'.format(days=7, max_rows=5),
+            method='GET',
+            headers=headers)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_error_reports_get(self):
+        """Test case for error_reports_get
+
+        Get errors reportings by conditions
+        """
+        headers = { 
+            'Accept': 'application/json',
+            "x-api-key": config.API_KEY,
+        }
+        response = self.client.open(
+            '/error-reports?days={days}&max_rows={max_rows}'.format(days=7, max_rows=20),
             method='GET',
             headers=headers)
         self.assert200(response,
