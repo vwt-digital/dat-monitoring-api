9e7ef0bc87a9e68ac193836658143527fe589d02
dvanrhenen <d.vanrhenen@vwt.digital>
2020-05-26 16:52:05 +0200

    Removed retrieval of keys-only. DAT-3001

--- functions/correct_cloudbuilds_func/main.py
+++ functions/correct_cloudbuilds_func/main.py
@@ -18,7 +18,6 @@ def correct_cloudbuilds(request):
         time_delta = (datetime.datetime.now() - datetime.timedelta(hours=interval)).isoformat()
         query.add_filter(request.args["field"], "<=", time_delta)
         query.add_filter("status", "=", "pending")
-        query.keys_only()
         entities = query.fetch()
 
         batch_count_total = 0
