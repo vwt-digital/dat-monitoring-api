2f5e0cb9053c83d02eaddef889619d68f591f289
JelleBwns <jellebouwens@hotmail.com>
2020-12-11 16:55:59 +0100

    DAT-5023 Publish to ops-issues

--- functions/consume_errors_func_ops_issues/main.py
+++ functions/consume_errors_func_ops_issues/main.py
@@ -1,6 +1,11 @@
 import base64
 import json
 import logging
+import config
+
+from google.cloud import pusbsub_v1
+
+publisher = pusbsub_v1.PublisherClient()
 
 
 def topic_to_topic(request):
@@ -9,6 +14,25 @@ def topic_to_topic(request):
     envelope = json.loads(request.data.decode('utf-8'))
     payload = base64.b64decode(envelope['message']['data'])
 
-    logging.info(payload)
+    # Extract subscription from subscription string
+    try:
+        subscription = envelope['subscription'].split('/')[-1]
+        logging.info(f'Message received from {subscription} [{payload}]')
 
+        # Publish to ops-issues here
+        topic_path = publisher.topic_path(config.ODH_PROJECT, config.OPS_ISSUES)
+        future = publisher.publish(topic_path, json.dumps(payload).encode('utf-8'))
+        future.add_done_callback(
+            lambda x: logging.info(
+                'Published msg with ID {}'.format(
+                    future.result()))
+        )
+
+    except Exception as e:
+        logging.info('Extract of subscription failed')
+        logging.debug(e)
+        return 'Conflict', 409
+
+    # Returning any 2xx status indicates successful receipt of the message.
+    # 204: no content, delivery successfull, no further actions needed
     return 'OK', 204
