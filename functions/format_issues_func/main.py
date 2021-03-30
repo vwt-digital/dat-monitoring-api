5db25fcef97f97e2dc2c643f008e75affa4e471b
MaxvanderWeide <max.vanderweide@student.hu.nl>
2021-03-30 14:08:50 +0200

    Nonetype variables for titles will now cause 422 error DAT-6111

--- functions/format_issues_func/main.py
+++ functions/format_issues_func/main.py
@@ -2,10 +2,9 @@ import base64
 import json
 import logging
 from functools import reduce
-from gobits import Gobits
 
 import config
-
+from gobits import Gobits
 from google.cloud import pubsub_v1
 
 publisher = pubsub_v1.PublisherClient()
@@ -14,18 +13,24 @@ publisher = pubsub_v1.PublisherClient()
 def get_issue_title(title_type, payload):
     issue_type = config.ISSUE_TITLES[title_type]
     variables = {}
-    for key, value in issue_type['variables'].items():
-        variables[key] = get_variable_value(payload=payload, keys=value)
-    return issue_type['title'].format(**variables)
+    for key, value in issue_type["variables"].items():
+        variable_value = get_variable_value(payload=payload, keys=value)
+        if variable_value is None:
+            # Makes sure that no invalid title can pass.
+            return None
+        variables[key] = variable_value
+    return issue_type["title"].format(**variables)
 
 
 def check_conditions(title_type, payload):
     issue_type = config.ISSUE_TITLES[title_type]
-    if 'conditions' not in issue_type:
-        return True
-    for condition in issue_type['conditions']:
-        if get_variable_value(payload=payload, keys=condition['variable']) not in condition['shouldBe']:
-            return False
+    if "conditions" in issue_type:
+        for condition in issue_type["conditions"]:
+            if (
+                get_variable_value(payload=payload, keys=condition["variable"])
+                not in condition["shouldBe"]
+            ):
+                return False
     return True
 
 
@@ -35,26 +40,43 @@ def get_variable_value(payload, keys):
 
 def topic_to_topic(request):
     # Extract data from request
-    envelope = json.loads(request.data.decode('utf-8'))
-    payload = json.loads(base64.b64decode(envelope['message']['data']))
+    envelope = json.loads(request.data.decode("utf-8"))
+    payload = json.loads(base64.b64decode(envelope["message"]["data"]))
 
     # Extract subscription from subscription string
     try:
-        subscription = envelope['subscription'].split('/')[-1]
-        logging.info(f'Message received from {subscription} {payload}')
+        subscription = envelope["subscription"].split("/")[-1]
+        logging.info(f"Message received from {subscription} {payload}")
     except Exception as e:
-        logging.info('Extract of subscription failed')
+        logging.info("Extract of subscription failed")
         logging.exception(e)
-        return 'Conflict', 409
-    if not hasattr(config, 'ISSUE_TITLES') or subscription not in config.ISSUE_TITLES:
-        logging.info(f'Not formatting issue from {subscription} because of missing title subscription.')
+        return "Conflict", 409
+    if not hasattr(config, "ISSUE_TITLES") or subscription not in config.ISSUE_TITLES:
+        logging.info(
+            f"Not formatting issue from {subscription} because of missing title subscription."
+        )
     elif not check_conditions(title_type=subscription, payload=payload):
-        logging.info(f'Not formatting issue from {subscription} because of failing conditions.')
+        logging.info(
+            f"Not formatting issue from {subscription} because of failing conditions with {payload}"
+        )
     else:
         title = get_issue_title(title_type=subscription, payload=payload)
 
-        formatted = json.dumps({"gobits": [Gobits().to_json()], "issue": {"title": title, "payload": payload}},
-                               indent=2).encode('utf-8')
+        if not title:
+            return (
+                f"Failed to create title with {payload}."
+                f"This is most likely not caused by the config, "
+                f"but by an invalid payload provided by an external service.",
+                422,
+            )
+
+        formatted = json.dumps(
+            {
+                "gobits": [Gobits().to_json()],
+                "issue": {"title": title, "payload": payload},
+            },
+            indent=2,
+        ).encode("utf-8")
 
         # Publish to ops-issues here
         topic_path = publisher.topic_path(config.ODH_PROJECT, config.OPS_ISSUES)
@@ -62,4 +84,4 @@ def topic_to_topic(request):
 
     # Returning any 2xx status indicates successful receipt of the message.
     # 204: no content, delivery successful, no further actions needed
-    return 'OK', 204
+    return "OK", 204
