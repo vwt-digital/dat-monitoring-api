c85c9ec2a2ffc31d7ebb54970cba20447805c63a
SpringPie <bente.vandepol@gmail.com>
2021-05-21 17:18:16 +0200

    Adding the option to have a root in the payload DAT-6521 [skip ci]

--- functions/format_issues_func/main.py
+++ functions/format_issues_func/main.py
@@ -63,35 +63,54 @@ def topic_to_topic(request):
             f"Not formatting issue from {subscription} because of failing conditions with {payload}"
         )
     else:
-        title = get_issue_title(title_type=subscription, payload=payload)
-
-        if not title:
-            logging.exception(
-                f"Failed to create title with {payload}."
-                f"This is most likely not caused by the config, "
-                f"but by an invalid payload provided by an external service."
-            )
-            return (
-                "Unprocessable Entity",
-                422,
-            )
-
-        formatted = json.dumps(
-            {
-                "gobits": [Gobits().to_json()],
-                "issue": {
-                    "title": title,
-                    "category": config.ISSUE_TITLES[subscription].get("category", ""),
-                    "payload": payload,
-                },
-            },
-            indent=2,
-        ).encode("utf-8")
-
-        # Publish to ops-issues here
-        topic_path = publisher.topic_path(config.ODH_PROJECT, config.OPS_ISSUES)
-        publisher.publish(topic_path, formatted)
+        # Check if there is a root in the payload
+        root = config.ISSUE_TITLES[subscription].get("root", "")
+        if root:
+            # Check if root of payload is a list
+            root_payload = payload.get(root)
+            if not root_payload:
+                logging.error(
+                    f"A root was defined in the config for subscription {subscription} but it could not be found in the payload"
+                )
+            if isinstance(root_payload, list):
+                for sub_payload in root_payload:
+                    publish(subscription, sub_payload)
+            else:
+                publish(subscription, root_payload)
+        else:
+            publish(subscription, payload)
 
     # Returning any 2xx status indicates successful receipt of the message.
     # 204: no content, delivery successful, no further actions needed
     return "OK", 204
+
+
+def publish(subscription, payload):
+    title = get_issue_title(title_type=subscription, payload=payload)
+
+    if not title:
+        logging.exception(
+            f"Failed to create title with {payload}."
+            f"This is most likely not caused by the config, "
+            f"but by an invalid payload provided by an external service."
+        )
+        return (
+            "Unprocessable Entity",
+            422,
+        )
+
+    formatted = json.dumps(
+        {
+            "gobits": [Gobits().to_json()],
+            "issue": {
+                "title": title,
+                "category": config.ISSUE_TITLES[subscription].get("category", ""),
+                "payload": payload,
+            },
+        },
+        indent=2,
+    ).encode("utf-8")
+
+    # Publish to ops-issues here
+    topic_path = publisher.topic_path(config.ODH_PROJECT, config.OPS_ISSUES)
+    publisher.publish(topic_path, formatted)
