76e8003209941c0d0a478419c5d0e8c438acb1c5
SpringPie <bente.vandepol@gmail.com>
2021-05-25 13:18:36 +0200

    Adjusting test to get API key via environment DAT-6601 [skip ci]

--- api_server/openapi_server/test/test_default_controller.py
+++ api_server/openapi_server/test/test_default_controller.py
@@ -1,66 +1,75 @@
 # coding: utf-8
 
 from __future__ import absolute_import
-import unittest
-import config
 
-from openapi_server.models.build_trigger_status import BuildTriggerStatus  # noqa: E501
-from openapi_server.models.error_report_count import ErrorReportCount  # noqa: E501
-from openapi_server.models.error_report_response import ErrorReportResponse  # noqa: E501
+import os
+import unittest
+
+from openapi_server.models.build_trigger_status import \
+    BuildTriggerStatus  # noqa: E501
+from openapi_server.models.error_report_count import \
+    ErrorReportCount  # noqa: E501
+from openapi_server.models.error_report_response import \
+    ErrorReportResponse  # noqa: E501
 from openapi_server.test import BaseTestCase
 
 
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
+    def __init__(self):
+        self.api_key = os.environ.get("API_KEY")
+
     def test_build_statuses_triggers_get(self):
         """Test case for build_statuses_triggers_get
 
         Get all build trigger statuses
         """
-        headers = { 
-            'Accept': 'application/json',
-            "x-api-key": config.API_KEY,
+        headers = {
+            "Accept": "application/json",
+            "x-api-key": self.api_key,
         }
         response = self.client.open(
-            '/build-statuses-triggers',
-            method='GET',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+            "/build-statuses-triggers", method="GET", headers=headers
+        )
+        self.assert200(response, "Response body is : " + response.data.decode("utf-8"))
 
     def test_error_reports_counts_get(self):
         """Test case for error_reports_counts_get
 
         Get count of project errors reportings in last x days
         """
-        headers = { 
-            'Accept': 'application/json',
-            "x-api-key": config.API_KEY,
+        headers = {
+            "Accept": "application/json",
+            "x-api-key": self.api_key,
         }
         response = self.client.open(
-            '/error-reports/counts?days={days}&max_rows={max_rows}'.format(days=7, max_rows=5),
-            method='GET',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+            "/error-reports/counts?days={days}&max_rows={max_rows}".format(
+                days=7, max_rows=5
+            ),
+            method="GET",
+            headers=headers,
+        )
+        self.assert200(response, "Response body is : " + response.data.decode("utf-8"))
 
     def test_error_reports_get(self):
         """Test case for error_reports_get
 
         Get errors reportings
         """
-        headers = { 
-            'Accept': 'application/json',
-            "x-api-key": config.API_KEY,
+        headers = {
+            "Accept": "application/json",
+            "x-api-key": self.api_key,
         }
         response = self.client.open(
-            '/error-reports?page_size={page_size}&page={page}'.format(page_size=5, page='next'),
-            method='GET',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+            "/error-reports?page_size={page_size}&page={page}".format(
+                page_size=5, page="next"
+            ),
+            method="GET",
+            headers=headers,
+        )
+        self.assert200(response, "Response body is : " + response.data.decode("utf-8"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
